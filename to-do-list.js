089ebeb9ebf6ce1c9aa652bbe044e7ce2caf18d0
Gani Ko <ganiko.dev@gmail.com>
2023-10-10 06:38:42 +0200

    change color alert

--- to-do-list.js
+++ to-do-list.js
@@ -84,7 +84,7 @@ function addItem(item){
 
     let alertDom = document.querySelector('#alert')
 
-    const ALERT = (message) =>`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    const ALERT = (message) =>`<div class="alert alert-danger alert-dismissible fade show" role="alert">
     <strong>HEY :)</strong>${message}
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
